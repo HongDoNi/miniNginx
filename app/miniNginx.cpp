e450f62d61b440d8881baa3db09399355426e8e8
HongDoNi <zhangyimsdn@foxmail.com>
2023-12-13 16:07:04 +0800

    3.7 daemon

--- app/miniNginx.cpp
+++ app/miniNginx.cpp
@@ -3,6 +3,8 @@
 #include <sys/signal.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 
 void sig_usr(int signo) {
@@ -29,63 +31,53 @@ char **g_os_argv;
 char *gp_envmem = nullptr;
 int g_value = 0;
 
-int main(const int argc, const char* const * argv) {
-    g_os_argv = (char**) argv;
+int ngx_daemon() {
+    int fd;
 
-    pid_t pid;
-
-    sigset_t newmask, oldmask, pendmask;
-
-    if(signal(SIGUSR1, sig_usr) == SIG_ERR) {
-        printf("SIGUSR1 error\n");
-    }
-
-    if(signal(SIGUSR2, sig_usr) == SIG_ERR) {
-        printf("SIGUSR2 error\n");
-    }
-
-    if(signal(SIGQUIT, sig_usr) == SIG_ERR) {
-        printf("SIGQUIT error\n");
-    }
-    
-
-    sigemptyset(&newmask);
-    // 把newmask信号集置零
-
-    sigaddset(&newmask, SIGQUIT);
-    // 把newmask信号集中的sigquit位置1，屏蔽该信号
-
-    // sigprocmask()
-    sigprocmask(SIG_BLOCK, &newmask, &oldmask);
-    // SIG_BLOCK 意思是设置信号集，把当前信号集设为newmask，oldmask用于保存原有信号集
-
-    pid = fork();
-    
+    int pid = fork();
     if(pid < 0) {
-        printf("创建子进程失败\n");
-        exit(1);
+        printf("fork fail\n");
+        return 1;
     }
     else if(pid == 0) {
-        while(1) {
-            g_value += 10;
-            sleep(1);
-            printf("子进程, pid: %d, g_value: %d\n", getpid(), g_value);
-        }
-        
-        
+        printf("子进程, pid : %d \n", getpid());
     }
     else {
-        while(1) {
-            g_value += 1;
-            sleep(1);
-            printf("父进程, pid: %d, g_value: %d\n", getpid(), g_value);
-        }
-        
+        printf("父进程, pid : %d \n", getpid());
+        exit(0);
     }
 
-    if(signal(SIGCHLD, sig_usr) == SIG_ERR) {
-        printf("SIGCHLD error\n");
+    setsid();
+    
+    umask(0);
+
+    fd = open("/dev/null", O_RDWR);
+
+    if(dup2(fd, STDIN_FILENO) == -1) exit(1);
+    if(dup2(fd, STDOUT_FILENO) == -1) exit(1);
+
+    if(fd > STDERR_FILENO) {
+        close(fd);
     }
+
+
+
+
+
+    
+
+
+    return 0;
+
+
+}
+
+int main(const int argc, const char* const * argv) {
+
+
+    g_os_argv = (char**) argv;
+
+    ngx_daemon();
 
     while(1) {
         sleep(1);
